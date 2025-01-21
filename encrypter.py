56b87b1c34c76f099b1a7099d695ab187c4532a0
osvaldosdj <31571091+osvaldosdj@users.noreply.github.com>
2025-01-21 11:43:26 -0300

    Update encrypter.py

--- encrypter.py
+++ encrypter.py
@@ -11,7 +11,7 @@ file.close()
 os.remove(file_name)
 
 ## chave de criptografia
-key = b"minhasenhadedescriptografia"
+key = b"minhasenhadedesc"
 aes = pyaes.AESModeOfOperationCTR(key)
 
 ## criptografar o arquivo
